b29e640f0675637a25bd0471d3747e86d60938e6
KHsuan <50944459+KHsuan@users.noreply.github.com>
2022-03-02 14:54:20 +0800

    message

--- app.py
+++
@@ -1,6 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-serverIP = os.getenv("SERVER_IP")
-print(serverIP)
--- name.py
+++
@@ -1,9 +0,0 @@
-import os
-import math
-# from dotenv import load_dotenv
-
-# print("Before load_dotenv()", os.getenv("DBCONN_STR"))
-# load_dotenv()
-# print("After load_dotenv()", os.getenv("DBCONN_STR"))
-
-help("list")
